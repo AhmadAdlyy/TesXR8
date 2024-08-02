19f98a431dacfc512b982b360bb0fdbf349b29fa
AhmadAdlyy <ahmad_maulana_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:00:57 +0700

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,22 @@
+public class Abc {
+    public static void main(String[] args) {
+        String nama= "Adly";
+        String asalkota="Malang";
+        int Usia=15;
+        double Beratbadan=95.00;
+        float Tinggibadan=150.0f;
+        char jeniskelamin='p';
+        boolean status=false;
+
+        System.out.println("Nama: "+nama);
+        System.out.println("Usia: "+Usia);
+        System.out.println("Berat Badan: "+Beratbadan);
+        System.out.println("Tinggi Badan: "+Tinggibadan);
+        System.out.println("Jenis Kelamin: "+jeniskelamin);
+        System.out.println("status: "+status);
+        
+
+        
+    }
+    
+}
