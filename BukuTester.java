2b60b5f30b46e4afd8f2e6199b762e897f9ce7b8
AhmadAdlyy <ahmad_maulana_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 15:16:07 +0700

    Add files via upload

---
+++ BukuTester.java
@@ -0,0 +1,10 @@
+public class BukuTester {
+    public static void main(String[] args){
+        Buku cinta = new Buku(0000, "apa itu cinta?", "dlyxtoxz", "2021");
+        Buku agama = new Buku(8989, "PAI", "ADLY DAH", "2020");
+        Buku sejarah = new Buku(9876, "SKI", "angin miber", "2000");
+        Buku komik = new Buku(1234, "aneh brok", "geblekkers", "2023");
+        Buku novel = new Buku(3214, "cinta yang aneh", "loro ati", "2022");
+        
+    }
+}
